be8f4996e68a5a3dd9ba266d2d18d63fb20926c2
Eshy10 <adewalebose15@gmail.com>
2020-09-21 16:06:48 +0100

    add initial state and pass the store to component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 import './index.css';
 import App from './components/App';
 
+const initialState = {
+  books: [
+    {
+      id: Math.floor(Math.random() * 10),
+      title: 'Vine',
+      category: 'Horror',
+    },
+    {
+      id: Math.floor(Math.random() * 10),
+      title: 'The mountains',
+      category: 'Sci-Fi',
+    },
+    {
+      id: Math.floor(Math.random() * 10),
+      title: 'Rebels',
+      category: 'Action',
+    },
+  ],
+};
+
+const store = createStore(rootReducer, initialState);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
